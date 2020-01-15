5e2dc7b7d8f644ceb5178281e9cf428203c03258
Raphaël Lavoie <rapolysec@hotmail.com>
2020-01-14 20:41:32 -0500

    Système de tuiles de base + demo dans SampleScene

---
+++ Assets/Script/GridBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridBehavior : MonoBehaviour
+{
+    private List<GameObject> m_Tiles = new List<GameObject>();
+    public int m_GridSizeX = 0, m_GridSizeY = 0;
+    //public GameObject[m_GridSizeX][m_GridSizeY];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            m_Tiles.Add(child.gameObject);
+        }
+        for (int i = 0; i < m_Tiles.Count; i++)
+        {
+            if(i > m_GridSizeX)
+            {
+                m_Tiles[i].GetComponent<TileBehavior>().SetSouthTile(m_Tiles[i - m_GridSizeX]);
+            }  
+            if(i + m_GridSizeX < m_Tiles.Count)
+            {
+                Debug.Log(i);
+                m_Tiles[i].GetComponent<TileBehavior>().SetNorthTile(m_Tiles[i + m_GridSizeX]);
+            }
+            if(i - 1 >= 0 && i % m_GridSizeX != 0)
+            {
+                m_Tiles[i].GetComponent<TileBehavior>().SetWestTile(m_Tiles[i - 1]);
+            }
+            if (i + 1 % m_GridSizeX != 0 && i + 1 < m_Tiles.Count)
+            {
+                m_Tiles[i].GetComponent<TileBehavior>().SetEastTile(m_Tiles[i + 1]);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/TileBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileBehavior : MonoBehaviour
+{
+    public enum m_TileTypeEnum {WALKABLE, OBSTACLE};
+    public m_TileTypeEnum m_TileType;
+    [SerializeField]
+    private GameObject m_NorthTile;
+    [SerializeField]
+    private GameObject m_EastTile;
+    [SerializeField]
+    private GameObject m_SouthTile;
+    [SerializeField]
+    private GameObject m_WestTile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetNorthTile(GameObject a_Tile)
+    {
+        m_NorthTile = a_Tile;
+    }
+    public void SetEastTile(GameObject a_Tile)
+    {
+        m_EastTile = a_Tile;
+    }
+    public void SetSouthTile(GameObject a_Tile)
+    {
+        m_SouthTile = a_Tile;
+    }
+    public void SetWestTile(GameObject a_Tile)
+    {
+        m_WestTile = a_Tile;
+    }
+}
