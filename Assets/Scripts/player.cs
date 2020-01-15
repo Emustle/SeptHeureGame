e167c95c3b951070e39d6ae2bd5219127b5da786
Hy-Vong <hyvong.georges@gmail.com>
2020-01-14 21:59:40 -0500

    Deplacement de base du jouer a ameliorer

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -4,29 +4,69 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    private float m_RotationDegrees;
-
-    private Vector3 m_FacedDirection;
     private Vector3 m_DesiredDirection;
+    private Vector3 m_NextPos;
 
-    [SerializeField] private float m_RotSpeed;
+    private Quaternion m_TargetRot;
+    private float m_RotSpeed;
+
+    private Rigidbody m_Rb;
+    private bool m_IsMoving;
+    private bool m_IsNextPosSet;
 
     // Start is called before the first frame update
     void Start()
     {
+        m_RotSpeed = 600f;
+        m_Rb = GetComponent<Rigidbody>();
 
+        m_NextPos = transform.position;
+        m_IsMoving = false;
+        m_IsNextPosSet = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //ROTATION ET DEPLACEMENT
+
         float t_HorizontalDir = Input.GetAxis("Horizontal");
         float t_VerticalDir = Input.GetAxis("Vertical");
 
-        m_DesiredDirection = new Vector3(t_HorizontalDir, 0, t_VerticalDir);
+        if ((t_HorizontalDir != 0 || t_VerticalDir != 0) && !m_IsMoving)
+        {
+            if (t_HorizontalDir != 0)
+                t_VerticalDir = 0;
+            if (t_VerticalDir != 0)
+                t_HorizontalDir = 0;
 
-        Quaternion rot = Quaternion.LookRotation(m_DesiredDirection, Vector3.up);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, Time.deltaTime*m_RotSpeed);
-        
+            m_DesiredDirection = new Vector3(t_HorizontalDir, 0, t_VerticalDir);
+            m_IsMoving = true;
+        }
+
+        m_TargetRot = Quaternion.LookRotation(m_DesiredDirection, Vector3.up);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, m_TargetRot, Time.deltaTime*m_RotSpeed);
+
+        if (Mathf.Abs(transform.rotation.eulerAngles.y) % 90 == 0 && m_IsMoving)
+        {
+            if (!m_IsNextPosSet)
+            {
+                m_IsNextPosSet = true;
+                m_NextPos = transform.position + transform.forward * 2;
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, m_NextPos, 5 * Time.deltaTime);
+            }
+
+            if (Vector3.Distance(transform.position, m_NextPos) < 0.01)
+            {
+                m_IsMoving = false;
+                m_IsNextPosSet = false;
+            }
+
+        }
+
     }
+
 }
