3371d31947ca27bc4799c7a1d4d1420bd3ce88fa
Raphaël Lavoie <rapolysec@hotmail.com>
2020-01-15 11:41:43 -0500

    Ajustements rotation jour/nuit + horloge 100% fonctionelle

--- Assets/Scripts/CameraBehavior.cs
+++ Assets/Scripts/CameraBehavior.cs
@@ -8,6 +8,7 @@ public class CameraBehavior : MonoBehaviour
     public GameObject player;
     private Vector3 m_Offset;
     private GameObject m_BlackScreen;
+    private GameObject m_Needle;
 
     public static CameraBehavior m_Instance;
     public static CameraBehavior Instance
@@ -28,6 +29,7 @@ public class CameraBehavior : MonoBehaviour
     void Start()
     {
         m_BlackScreen = gameObject.transform.Find("Canvas").transform.Find("BlackScreen").gameObject;
+        m_Needle = gameObject.transform.Find("Canvas").transform.Find("Clock").transform.Find("Needle").gameObject;
         m_Offset = new Vector3(0, 2.5f, -2.5f);
         Color t_color = m_BlackScreen.GetComponent<RawImage>().color;
         m_BlackScreen.GetComponent<RawImage>().color = new Color(t_color.r, t_color.g, t_color.b, 255);
@@ -63,5 +65,11 @@ public class CameraBehavior : MonoBehaviour
         
         
         //Debug.Log(m_UiHurtScreen.GetComponent<RawImage>().color);
+    }
+
+    public void TickClockNeedle(float Progress)
+    {
+        //Debug.Log(Progress);
+        m_Needle.transform.eulerAngles = new Vector3(0,0,Mathf.Lerp(60, 240, 1 - Progress));
     }
 }
--- Assets/Scripts/RotationJourNuit.cs
+++ Assets/Scripts/RotationJourNuit.cs
@@ -6,7 +6,13 @@ public class RotationJourNuit : MonoBehaviour
 {
     public int timer;
     float rotationSpeed = -1.25f;
-    Vector3 currentEulerAngles = new Vector3 (50,0,0);
+    private float m_RotationToGo;
+    private float m_FullRotation;
+    private float m_InitialRotation;
+    [SerializeField]
+    private float m_TargetRotation = -25f;
+    private Vector3 m_CurrentEulerAngles;
+    //Vector3 currentEulerAngles = new Vector3 (50,0,0);
     Quaternion currentRotation;
     float x;
     float y;
@@ -14,7 +20,11 @@ public class RotationJourNuit : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if(timer == 0)
+        m_InitialRotation = transform.rotation.eulerAngles.x;
+        m_FullRotation = m_InitialRotation - m_TargetRotation;
+        m_RotationToGo = m_FullRotation;
+        m_CurrentEulerAngles = new Vector3(m_InitialRotation, 0, 0);
+        if (timer == 0)
         {
             Debug.Log("je sais pas ce que je fais");
         }
@@ -23,22 +33,26 @@ public class RotationJourNuit : MonoBehaviour
     }
     void Update()
     {
+
         //modifying the Vector3, based on input multiplied by speed and time
-        currentEulerAngles += new Vector3(x, y, z) * Time.deltaTime * rotationSpeed;
+        m_CurrentEulerAngles += new Vector3(x, y, z) * Time.deltaTime * rotationSpeed;
 
         //Réduire l'intensité
         GetComponent<Light>().intensity -= Time.deltaTime * 0.0125f;
 
         //moving the value of the Vector3 into Quanternion.eulerAngle format
-        currentRotation.eulerAngles = currentEulerAngles;
+        currentRotation.eulerAngles = m_CurrentEulerAngles;
 
         //apply the Quaternion.eulerAngles change to the gameObject
         transform.rotation = currentRotation;
         //modifying the Vector3, based on input multiplied by speed and time
-        if(currentEulerAngles.x <= -25f)
+        m_RotationToGo = m_CurrentEulerAngles.x + 25;
+        if (m_CurrentEulerAngles.x <= -25f)
         {
             rotationSpeed = 0;
         }
+
+        CameraBehavior.Instance.TickClockNeedle(m_RotationToGo / m_FullRotation);
         
     }
 
