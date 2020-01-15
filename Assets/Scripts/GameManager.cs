49e000eec57ac9c41074b41bc09cfc27bf6117ba
UnkoRyuu <quentin.bernard24@orange.fr>
2020-01-15 14:06:58 -0500

    menu implemeté

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject MenuCanvas;
+    public GameObject SurDeQuitterCanvas;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        SurDeQuitterCanvas.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+    public void play()
+    {
+        Debug.Log("test");
+        
+        SceneManager.LoadScene("histoire");
+    }
+    public void SurDeQuitter()
+    {
+        SurDeQuitterCanvas.SetActive(true);
+        MenuCanvas.SetActive(false);
+    }
+    public void CancelQuitter()
+    {
+        SurDeQuitterCanvas.SetActive(false);
+        MenuCanvas.SetActive(true);
+    }
+    public void QuitterVriament()
+    {
+        Application.Quit();
+    }
+    
+    
+}
---
+++ Assets/Scripts/gameManagerInGame.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameManagerInGame : MonoBehaviour
+{
+    public GameObject MenuPause;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MenuPause.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            PauseMenu();
+        }
+        
+    }
+    public void PauseMenu()
+    {
+        Time.timeScale = 0f;
+        MenuPause.SetActive(true);
+        Debug.Log("PauseMenu");
+    }
+
+    public void retourMenu()
+    {
+        Debug.Log("retour menu");
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("menu");
+    }
+
+    public void retourGame()
+    {
+        Debug.Log("retour menu");
+        MenuPause.SetActive(false);
+        Time.timeScale = 1f;
+
+    }
+}
