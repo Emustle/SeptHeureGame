d7db15c14909f932a312e7def0397f53dd028f59
Hy-Vong <hyvong.georges@gmail.com>
2020-01-15 02:24:55 -0500

    Ajout flashlight joueur avec ajustement selon la directional light

---
+++ Assets/Scripts/flashlightBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flashlightBehavior : MonoBehaviour
+{
+    private GameObject m_MainLight;
+    private float m_DesiredIntensity;
+    private Light[] m_LightComps;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_LightComps = GetComponentsInChildren<Light>();
+        foreach(Light t_light in m_LightComps)
+        {
+            t_light.intensity = 0f;
+        }
+        m_DesiredIntensity = 0.8f;
+        m_MainLight = GameObject.Find("Directional Light");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AjustIntensity();
+    }
+
+    private void AjustIntensity()
+    {
+        foreach(Light t_light in m_LightComps)
+        {
+            t_light.intensity = (1 - m_MainLight.GetComponent<Light>().intensity) * m_DesiredIntensity;
+        }
+    }
+}
